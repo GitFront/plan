cd59a8fc3dbe2abdb574b6df74bd70018ea2093e
liuqinghua <liuqinghua@aspirecn.com>
2018-05-25 14:45:28 +0800

    人生中的第一个App

--- index.js
+++ index.js
@@ -8,4 +8,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 //registerComponent方法 第一个参数 我们应用程序的名称,即项目的名称 第二个参数入口组件对象
-AppRegistry.registerComponent('plan', () => App);
+AppRegistry.registerComponent('planObject', () => App);
